d521313260e4083be467b7d41729b1b0c209da9d
Van Chien <vanchien120104@gmail.com>
2025-07-26 09:51:32 +0700

    update

--- EmployeeManagement/obj/Debug/net7.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Debug/net7.0/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47937143616331e6080c42525b8b7f80a211f69b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
